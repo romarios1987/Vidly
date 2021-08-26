15a55a5974949838015c37ee10bb6c2bed91823f
romarios1987 <romios047@gmail.com>
2021-08-26 08:58:50 +0300

    initial project

---
+++ src/fontawesome.js
@@ -0,0 +1,9 @@
+// import the library
+import {library} from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+    faHeart
+);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import './fontawesome';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
